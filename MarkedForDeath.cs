60b7b2d52e0b6a8c84d74a8972ca274173fdb084
Maiko Tuitupou <tuitupoumaiko@gmail.com>
2021-02-05 20:41:40 -0700

    [RELEASE] Version 1.0

--- MarkedForDeath.cs
+++ MarkedForDeath.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Oxide.Plugins
 {
-    [Info("MarkedForDeath", "humanalog", "b1.0")]
+    [Info("MarkedForDeath", "humanalog", "v1.0")]
     [Description("One person will be marked for death. After killing this person, the killer becomes the next who is marked for death.")]
     public class MarkedForDeath : RustPlugin
     {
