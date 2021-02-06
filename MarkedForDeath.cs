47d7bc7de0534243df9663d630f2d47c3a494716
Maiko Tuitupou <tuitupoumaiko@gmail.com>
2021-02-05 19:23:05 -0700

    [ADD] Initialization server hooks

--- MarkedForDeath.cs
+++ MarkedForDeath.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Oxide.Core;
+using Oxide.Core.Configuration;
+using Oxide.Core.Plugins;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,30 @@ namespace Oxide.Plugins
     [Description("One person will be marked for death. After killing this person, the killer becomes the next who is marked for death.")]
     public class MarkedForDeath : RustPlugin
     {
-        
+        #region Variables
+
+        #endregion
+
+        #region Server Hooks
+
+        protected override void LoadDefaultConfig() // Create the config file
+        {
+            Puts("Config for MarkedForDeath does not exist, creating one now.");
+            Config["DefaultMarkedPlayerID"] = (ulong)0u;
+            Config["DefaultMarkedPlayerName"] = "Unassigned";
+        }
+
+        void OnServerInitialized() // Create the plugin data file if it doesn't already exist
+        {
+            if (!Interface.Oxide.DataFileSystem.ExistsDatafile("MarkedForDeathData"))
+            {
+                DynamicConfigFile dataFile = Interface.Oxide.DataFileSystem.GetDatafile("MarkedForDeathData");
+                dataFile["MarkedPlayerSteamID"] = Convert.ToUInt64(Config["DefaultMarkedPlayerID"]);
+                dataFile["MarkedPlayerName"] = Config["DefaultMarkedPlayerName"];
+                dataFile.Save();
+            }
+        }
+
+        #endregion
     }
 }
