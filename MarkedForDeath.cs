d647ebb9d19b7a455c263ce11bb2b3e01a752cec
Maiko Tuitupou <tuitupoumaiko@gmail.com>
2021-02-05 18:57:01 -0700

    [INIT] Rust plugin barebones

---
+++ MarkedForDeath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins
+{
+    [Info("MarkedForDeath", "humanalog", "b1.0")]
+    [Description("One person will be marked for death. After killing this person, the killer becomes the next who is marked for death.")]
+    public class MarkedForDeath : RustPlugin
+    {
+        
+    }
+}
