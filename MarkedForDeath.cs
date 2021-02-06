946ed749b15d5686d3256838ab0231ece932a2bd
Maiko Tuitupou <tuitupoumaiko@gmail.com>
2021-02-05 19:31:09 -0700

    [ADD] InfoPanel integration

--- MarkedForDeath.cs
+++ MarkedForDeath.cs
@@ -38,5 +38,53 @@ namespace Oxide.Plugins
         }
 
         #endregion
+
+        #region InfoPanel Integration
+
+        [PluginReference]
+        Plugin InfoPanel; // Reference to InfoPanel plugin
+
+        private void Loaded()
+        {
+            if(InfoPanel)
+            {
+                InfoPanel.Call("SendPanelInfo", "RustCTF", new List<string> { "FlagHolderPanel" });
+                InfoPanel.Call("PanelRegister", "RustCTF", "FlagHolderPanel", FlagHolderPanelCfg);
+                InfoPanel.Call("RefreshPanel", "RustCTF", "FlagHolderPanel");
+            }
+        }
+
+        private static string FlagHolderPanelCfg = @"
+        {
+            ""AnchorX"": ""Right"",
+            ""AnchorY"": ""Bottom"",
+            ""Autoload"": true,
+            ""Available"": true,
+            ""BackgroundColor"": ""0 0 0 0.4"",
+            ""Dock"": ""TopRightDock"",
+            ""FadeOut"": 0.0,
+            ""Height"": 0.95,
+            ""Margin"": ""0 0 0 0.005"",
+            ""Order"": 7,
+            ""Text"": {
+                ""Align"": ""MiddleCenter"",
+                ""AnchorX"": ""Left"",
+                ""AnchorY"": ""Bottom"",
+                ""Available"": true,
+                ""BackgroundColor"": ""0 0 0 0.4"",
+                ""Content"": ""No Content"",
+                ""FadeIn"": 0.0,
+                ""FadeOut"": 0.0,
+                ""FontColor"": ""1 1 1 1"",
+                ""FontSize"": 14,
+                ""Height"": 0.95,
+                ""Margin"": ""0 0 0 0.005"",
+                ""Order"": 0,
+                ""Width"": 1.0
+            },
+            ""Width"": 1.0
+        }";
+
+        #endregion
     }
 }
